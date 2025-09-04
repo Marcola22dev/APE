56f8f1ae4b7ff939bec119f0d7704e717405e812
MACIO ALVES DE QUEIROZ <macio.queiroz@a.ucb.br>
2025-09-04 12:59:12 -0300

    attaula8_for

--- Aulas/aula_008/main.c
+++ Aulas/aula_008/main.c
@@ -17,11 +17,22 @@
 
 int main() {
 	
-	int cont=0;
+//		---contagem até zero---
 	
-	for(cont=1;cont<101;cont++){
+	int cont=0;
+	 
+	for(cont=10; cont>=0; cont--){
 		printf("%d\n", cont);
 	}
+/*	  ---contagem até dez---
+
+	int cont=0;
+	
+	for(cont=1;cont<11;cont++){
+		printf("%d\n", cont);
+	}
+*/
+
 	
 	return 0;
 }
